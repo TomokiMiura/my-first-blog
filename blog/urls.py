a18da12c80dcc8eab8411d3b1dfed0f3dd88a775
tomokimiura <3ura10moki@gmail.com>
2021-02-20 11:15:26 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    # /以降がないURLなら投稿リストを表示する
+    path('post/<int:pk>/', views.post_detail, name='post_detail'), 
+    #pkはプライマリーキー。post/数字なら投稿の詳細を表示する
 ]
